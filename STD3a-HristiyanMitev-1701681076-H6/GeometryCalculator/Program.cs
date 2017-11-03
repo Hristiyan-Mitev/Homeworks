aabefab4088b3c69290191fd9b073f72cc101acd
Hristiyan Mitev <eminem233@abv.bg>
2017-11-03 23:57:49 +0200

    Initial commit.

---
+++ STD3a-HristiyanMitev-1701681076-H6/GeometryCalculator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void triangle(double side, double height)
+        {
+            double area;
+            area = side * height / 2;
+            Console.WriteLine("The area of the triangle is = {0:0.00}", area);
+        }
+
+        static void square(double side)
+        {
+            double area;
+            area = side * side;
+            Console.WriteLine("The area of the square is = {0:0.00}", area);
+        }
+
+        static void degreeToRadians(int degree)
+        {
+            double radians;
+            const double pi = Math.PI;
+            radians = (pi / 180) * degree;
+            Console.WriteLine("Your degrees in radians are = " + radians);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option (triangle, square or radians)");
+            string Figure = Console.ReadLine();
+            if (Figure == "triangle")
+            {
+
+                Console.WriteLine("Enter side: ");
+                double TriangleSide = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter height: ");
+                double TriangleHeight = double.Parse(Console.ReadLine());
+                triangle(TriangleSide, TriangleHeight);
+            }
+
+
+            if (Figure == "square")
+            {
+
+                Console.WriteLine("Enter side: ");
+                double SquareSide = double.Parse(Console.ReadLine());
+                square(SquareSide);
+            }
+
+            if (Figure == "radians")
+            {
+                Console.WriteLine("Enter the degrees you want to transform in radians: ");
+                int degree = int.Parse(Console.ReadLine());
+                degreeToRadians(degree);
+            }
+
+        }
+    }
+}
---
+++ STD3a-HristiyanMitev-1701681076-H6/GetMax/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void getMax(int number1, int number2, int number3)
+        {
+            if (number1 > number2 && number1 > number3)
+            {
+                Console.WriteLine("The biggest number is " + number1);
+            }
+            else if (number2 > number1 && number2 > number3)
+            {
+                Console.WriteLine("The biggest number is " + number2);
+            }
+            else
+            {
+                Console.WriteLine("The biggest number is " + number3);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter first number: ");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            int second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter third number: ");
+            int third = int.Parse(Console.ReadLine());
+
+            getMax(first, second, third);
+
+
+        }
+    }
+}
